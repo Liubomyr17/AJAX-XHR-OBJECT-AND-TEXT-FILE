92c68196411447912c2191ded939f9a7ea4bea57
Izdepskyi <Ragnarlobrok91>
2019-09-01 13:40:56 +0300

    AJAX-XHR-OBJECT-AND-TEXT-FILE

--- ajax.js
+++ ajax.js
@@ -1,34 +1,25 @@
 const btn = document.querySelector('.btn');
 const info = document.querySelector('.info');
 
-function getData(url){
-
+function getData(url) {
     const ajax = new XMLHttpRequest();
-console.log(ajax);
+    console.log(ajax);
 
-ajax.open('GET', url, true);
+    ajax.open('GET', url, true);
 
-// ajax.onreadystatechange = function(){
-// if(this.status === 200 && this.readyState === 4){
-//       info.textContent = this.responseText;
-//  }
-// };
+    ajax.onload = function() {
+        if(this.status === 200) {
+            info.textContent = this.responseText;
+        } else {
+            console.log(this.statusText);
+        }
+    };
 
-ajax.onload = function(){
-   if(this.status === 200 ){
-       info.textContent = this.responseText;
-   }else{
-      console.log(this.statusText);
-   }
-};
-
-ajax.oneerror = function(){
-    console.log('there was an error');
+    ajax.oneerror = function() {
+        console.log('there was an error');
+    }
+    ajax.send();
 }
-
-ajax.send();
-}
-
-btn.addEventListener("click", function(){
+btn,addEventListener("click", function(){
     getData("info.txt");
 });
